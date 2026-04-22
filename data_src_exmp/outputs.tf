df372d3acffb94d22a7ca7c38b3cc598c28d9ef7
lucky varma <neelamlakshminarayana88@gmail.com>
2026-04-22 16:32:35 +0530

    added datasrc & login to machine output & and lifecycle

---
+++ data_src_exmp/outputs.tf
@@ -0,0 +1,16 @@
+output "public_ip" {
+  value = aws_instance.linux.public_ip
+
+}
+output "username" {
+  value = var.aws_instance_info.username
+}
+  
+
+
+output "connect_machine" {
+
+  value = "ssh ${var.aws_instance_info.username}@${aws_instance.linux.public_ip}"
+  
+ ##value = format("ssh %s%s ", var.aws_instance_info.username,aws_instance.linux.public_ip) 
+}
